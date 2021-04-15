63b7fac7dcb424ac02e21cbdd837a364a65edec2
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-04-15 16:22:50 +0200

    Integer framesize

--- streaming/values.py
+++ streaming/values.py
@@ -1,7 +1,7 @@
 import os
 framerate = 80; frametime = 1/framerate
 datarate = 300000
-framesize = datarate / framerate
+framesize = int(datarate / framerate + 0.5)
 # Use random data as frame
 # Can simply use same frame every time - no need to re-generate each iteration
 frame = os.urandom(framesize)
