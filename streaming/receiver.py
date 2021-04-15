4cc4a431fa1bcbd4119c33340d564871d574d7ab
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-04-15 16:26:50 +0200

    Syntax fix

--- streaming/receiver.py
+++ streaming/receiver.py
@@ -7,7 +7,7 @@ from clock import now_us
 if __name__ == "__main__":
 	myip = '192.168.3.2'
 	port = 50000
-	s = socket.socket((socket.AF_INET, socket.SOCK_STREAM))
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((myip, port))
 	for _ in range(1600):
 		frame = s.recv(framesize)
