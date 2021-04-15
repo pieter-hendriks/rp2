adcf74a2f2e6d8c89bc120cb72fbb2cff2459580
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-04-15 17:16:15 +0200

    removed old self-defined variables in sender

--- streaming/sender.py
+++ streaming/sender.py
@@ -5,18 +5,15 @@ import time
 from clock import now_us
 
 from values import frame, frametime, ackLength, loopLength
-
+from values import sender, receiver, port
 
 if __name__ == "__main__":
-	dest = '192.168.3.2'
-	src = '192.168.4.2'
-	port = 50000
 	# socket.SOCK_STREAM for tcp, socket.SOCK_DGRAM for UDP
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 0)
-	s.bind((src, port))
-	s.connect((dest, port))
+	s.bind((sender, port))
+	s.connect((receiver, port))
 	for i in range(loopLength):
 		start = now_us()
 		ret = s.sendall(frame)
