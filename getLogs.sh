7dcca853836d919f47a3366f73ee6d61e4e7f2a1
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-08-27 10:48:35 +0200

    improved getlogs, also makes parent dirs if needed

--- getLogs.sh
+++ getLogs.sh
@@ -1,5 +1,5 @@
-mkdir "/home/pieter/school/rp2/results/moving/$1"
-mkdir "/home/pieter/school/rp2/results/static/$1"
+mkdir -p "/home/pieter/school/rp2/results/moving/$1"
+mkdir -p "/home/pieter/school/rp2/results/static/$1"
 sshpass -p "rover" scp -r rover@10.1.75.85:/home/rover/pieter/streaming/log/* "/home/pieter/school/rp2/results/moving/$1"
 sshpass -p "rover" ssh rover@10.1.75.85 "rm -r /home/rover/pieter/streaming/log/*"
 cp -r /home/pieter/school/rp2/streaming/log/* "/home/pieter/school/rp2/results/static/$1"
