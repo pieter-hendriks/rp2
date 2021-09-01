092c3938cce9453de7d1eb43e48f71f5d63c40d4
Pieter Hendriks <pieter.hendriks@student.uantwerpen.be>
2021-09-01 15:33:51 +0200

    Shorten testing time

--- streaming/values.py
+++ streaming/values.py
@@ -12,7 +12,7 @@ runlocal = False
 
 class Configuration:
 	def __init__(self, useLAN, useRandomFrameData):
-		self.experimentTime = 20
+		self.experimentTime = 5
 		# Frame rate & time
 		self.framerate = 60
 		self.frametime = 1/self.framerate
